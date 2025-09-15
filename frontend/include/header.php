b92cb048ccdc63f32523e08ad0a58bf1c5ecc5be
donatoflemma <tato.flemma@gmail.com>
2025-09-15 16:31:13 +0200

    xycxcvbnm,.

--- frontend/include/header.php
+++ frontend/include/header.php
@@ -11,8 +11,11 @@ function NamePage($title)
     echo "<meta name='description' content=''>";
     echo "<meta name='viewport' content='width=device-width, initial-scale=1'>";
     echo "<link rel='stylesheet' href='/DBMS/frontend/css/style.css'>";
+    // LINK PER IL PACHETTO CSS DI BOOSTRAP
+    echo "<link href='https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css' rel='stylesheet'
+        integrity='sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB' crossorigin='anonymous'>";
     echo "</head>";
-    echo "<body>";
+    echo "<body style='background-color: antiquewhite; '>";
 }
 //../frontend/css/style.css
 ?>
